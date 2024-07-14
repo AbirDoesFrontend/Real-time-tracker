93eb7f7420f9437a58a761150c44a546397eb876
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2024-07-14 21:02:22 +0600

    Setup: EJS

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+// Express Setup
 const express = require("express");
 const app = express();
+// Express Setup
+
+
+// Socekt.io Setup
 const http = require("http");
+const path = require("path");
 
 const socketio = require("socket.io");
-
 const server = http.createServer(app);
-
 const io = socketio(server);
+// Socekt.io Setup
+
+// Ejs Setup
+app.set("view engine" , "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+// Ejs Setup
 
 app.get("/" , function(req , res) {
     res.send("Hey");
 })
 
-app.listen(3000);
+server.listen(3000);
